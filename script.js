0513b31d8ce34a3402c6a80b596ac52a05958ad6
pii2903 <152416977+pii2903@users.noreply.github.com>
2023-11-29 21:06:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var screen=document.querySelector(".exprssion")
+var buttons=document.querySelectorAll(".btn")
+screen.value=""
+buttons.forEach(function(button){
+    button.addEventListener("click",function(){
+        screen.value=screen.value+button.getAttribute("data")
+    })
+})
+
+function solve(){
+    var expr=screen.value
+    var result=eval(expr)
+    screen.value=result
+}
+function clearData(){
+    screen.value=""
+}
